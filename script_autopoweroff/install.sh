fd0257ffa7321b01c9dbaa2494664368ec7efd29
xansx <aslopez110@gmail.com>
2020-10-30 01:18:40 +0100

    Update

--- script_autopoweroff/install.sh
+++ script_autopoweroff/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ACTION: Install script poweroff_last for auto-poweroff if no users logged in 20 minutes
 # INFO: Automatic poweroff may be useful in public or shared computers to avoid left computers ON needlessly
-# DEFAULT: n
+# DEFAULT: y
 
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
